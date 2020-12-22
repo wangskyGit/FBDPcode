79c1ac0dbaa7e56d92ce615019b1c33991941e06
wangsky <wangsky@newbee.com>
2020-12-22 17:31:26 +0800

    lab4

--- lab4/lab4_pyspark/task4.py
+++ lab4/lab4_pyspark/task4.py
@@ -9,6 +9,9 @@ from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql.functions import pandas_udf, PandasUDFType     
 from pyspark.mllib.classification import SVMModel,SVMWithSGD
 from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
 if __name__ == "__main__":
     sc = SparkContext("local", "most popular item")
     spark = SparkSession.builder \
@@ -16,7 +19,6 @@ if __name__ == "__main__":
     .appName("task3") \
     .config('spark.sql.crossJoin.enabled','true')\
     .getOrCreate()
-
     Schema1=StructType(
         [
             StructField('user_id',DoubleType()),
@@ -42,29 +44,35 @@ if __name__ == "__main__":
     data=spark.read.format('csv').option('sep',',').option('header','false').load('/home/wangsky/FBDP/processed_data',schema=Schema1)
     label=spark.read.format('csv').option('sep',',').option('header','false').load('/home/wangsky/FBDP/train_format1.csv',schema=Schema2)
     train_data=label.join(data,['user_id','seller_id'],'left')
+    train_data.show(5)
     t=train_data.drop('gender').dropna(how='any').rdd
-    training,testing=t.randomSplit([0.7,0.3],seed=10)
-    training=training.map(lambda line: LabeledPoint(line[2],[line[3:]]))
-    testing=testing.map(lambda line: LabeledPoint(line[2],[line[3:]]))
+    
+   
+    re=pd.DataFrame(columns=['sample_seed','SVM_error','LR_error'])
+    training_split,testing_split=t.randomSplit([0.7,0.3],seed=10)
+    positive1=testing_split.filter(lambda l:l[2]==1).count()
+    training=training_split.map(lambda line: LabeledPoint(line[2],[line[3:]]))
+    testing=testing_split.map(lambda line: LabeledPoint(line[2],[line[3:]]))
     training.cache()
-    svm=SVMWithSGD.train(training,iterations=25)
-    predicted=training.map(lambda x:(x.label,svm.predict(x.features)))
-    print(predicted.take(5))
-    error=predicted.filter(lambda lp:lp[0]!=lp[1]).count()/float(training.count())
-    print('训练集error：{}'.format(error))
-    test_pre=testing.map(lambda x:(x.label,svm.predict(x.features)))
-    test_error=predicted.filter(lambda lp:lp[0]!=lp[1]).count()/float(testing.count())
-    print('测试集error：{}'.format(test_error))
-
-    test_label=spark.read.format('csv').option('sep',',').option('header','false').load('/home/wangsky/FBDP/test_format1.csv',schema=Schema2)
-    data2=test_label.join(data,['user_id','seller_id'],'left')
-    t2=data2.drop('gender').fillna(0).rdd
-    test_data1=t2.map(lambda line:(line[0],line[1],LabeledPoint(line[2],[line[3:]])))
-    test_data1.cache()
-    test_predicted=test_data1.map(lambda x:(x[0],x[1],svm.predict(x[2].features)))
-    test_predictedDF=test_predicted.toDF(Schema2)
-    print(test_predictedDF.show(5))
-    test_predictedDF.toPandas().to_csv("/home/wangsky/FBDP/out.csv", header=True,index=False)
+    testing.cache()
+    lr=LogisticRegressionWithLBFGS()
+    LR=lr.train(training,iterations=10)
+    SVM=SVMWithSGD.train(training,iterations=10)
+    predicted_svm=training.map(lambda x:(x.label,SVM.predict(x.features)))
+    predicted_lr=training.map(lambda x:(x.label,LR.predict(x.features)))
+    errorSVM=predicted_svm.filter(lambda lp:lp[0]!=lp[1]).count()/float(training.count())
+    errorLR=predicted_lr.filter(lambda lp:lp[0]!=lp[1]).count()/float(training.count())
+    positive=predicted_lr.filter(lambda x:x[1]==1).count()
+   
+    print('SVM训练集error：{}'.format(errorSVM))
+    print('LR训练集error：{}'.format(errorLR))
+    test_preSVM=testing.map(lambda x:(x.label,SVM.predict(x.features)))
+    test_error=test_preSVM.filter(lambda lp:lp[0]!=lp[1]).count()/float(testing.count())
+    test_preLR=testing.map(lambda x:(x.label,LR.predict(x.features)))
+    test_errorLR=test_preLR.filter(lambda lp:lp[0]!=lp[1]).count()/float(testing.count())
+    print('SVM测试集error：{}'.format(test_error))
+    print('LR测试集error：{}'.format(test_errorLR))
+    
     
     
     
